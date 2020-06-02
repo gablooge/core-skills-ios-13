255b24969deb7d2a2091fd8a457367ea22357646
Samsul Hadi <samsulhadikediri@gmail.com>
2020-06-02 19:22:56 +0700

    Initial Commit

---
+++ Core Skills/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Core Skills
+//
+//  Created by SAMSUL HADI on 02/06/20.
+//  Copyright © 2020 SAMSUL HADI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
